0aa592a519449a273e5d181e095d1d6414d417e3
Stefan Rachev <stef.rachev98@gmail.com>
2022-06-24 21:48:41 +0200

    Endpoints secured

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,24 +1,40 @@
 module.exports = (app) => {
   const posts = require("../controllers/post.controller.js");
   var router = require("express").Router();
+  const { authJwt } = require("../middleware/middleware.index");
 
   // Create a new Post
-  router.post("/", posts.create);
+  router.post("/", [
+    authJwt.verifyToken
+  ], posts.create);
 
   // Retrieve all posts
-  router.get("/", posts.findAll);
+  router.get("/", [
+    authJwt.verifyToken
+  ], posts.findAll);
 
   // Retrieve a single Post with id
-  router.get("/:id", posts.findOne);
+  router.get("/:id", [
+    authJwt.verifyToken
+  ], posts.findOne);
 
   // Update a Post with id
-  router.put("/:id", posts.update);
+  router.put("/:id", [
+    authJwt.verifyToken,
+    authJwt.verifyUser
+  ], posts.update);
 
   // Delete a Post with id
-  router.delete("/:id", posts.delete);
+  router.delete("/:id", [
+    authJwt.verifyToken,
+    authJwt.verifyUser
+  ], posts.delete);
 
   // Delete all posts
-  router.delete("/", posts.deleteAll);
+  router.delete("/", [
+    authJwt.verifyToken,
+    authJwt.isAdmin
+  ], posts.deleteAll);
 
   app.use("/api/posts", router);
 };
